36c5e373367b94ecb29f248f7ab95adc0a86223a
jsdmas <sdmas5730@gmail.com>
2023-07-16 16:51:43 +0900

    :sparkles: Feat : Router config & pages folder

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home/index'));
+const NotFound = lazy(() => import('./pages/NotFound/index'));
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={'Loading....'}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+export default Router;
