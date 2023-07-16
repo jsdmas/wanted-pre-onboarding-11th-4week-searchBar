e228fa549b313ed724fcd37abcf8a2de76abc662
jsdmas <sdmas5730@gmail.com>
2023-07-16 17:54:48 +0900

    :art: Style : notfound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,4 +1,16 @@
+import styled from '@emotion/styled';
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return <Wrapper>Not Found</Wrapper>;
 }
 export default NotFound;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: auto;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  font-size: x-large;
+`;
