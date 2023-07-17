24612d85015c8ddbb655d07871b72ed8377afa7e
jsdmas <sdmas5730@gmail.com>
2023-07-17 13:53:33 +0900

    :sparkles: Feat : create form context

---
+++ src/context/form.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export type FormState = {
+  keyword: string;
+};
+
+type FormSetState = React.Dispatch<React.SetStateAction<FormState>>;
+
+const FormStateContext = createContext<FormState | null>(null);
+const SetFormStateContext = createContext<React.Dispatch<React.SetStateAction<FormState>> | null>(null);
+
+export function FormProvider({ children }: { children: React.ReactNode }) {
+  const [state, setState] = useState<FormState | null>(null);
+
+  return (
+    <FormStateContext.Provider value={state}>
+      <SetFormStateContext.Provider value={setState as FormSetState}>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(state);
+          }}
+        >
+          {children}
+        </form>
+      </SetFormStateContext.Provider>
+    </FormStateContext.Provider>
+  );
+}
+
+export function useFormStateContext() {
+  const context = useContext(FormStateContext);
+
+  if (context == null) {
+    throw new Error(`Cannot find <FormStateContext.Provider>`);
+  }
+
+  return context;
+}
+
+export function useSetFormStateContext() {
+  const context = useContext(SetFormStateContext);
+
+  if (context == null) {
+    throw new Error(`Cannot find <SetFormStateContext.Provider>`);
+  }
+
+  return context;
+}
