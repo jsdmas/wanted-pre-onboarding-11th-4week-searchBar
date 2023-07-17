43058b1b386fca87beb1734f446925203087ceca
jsdmas <sdmas5730@gmail.com>
2023-07-17 22:08:58 +0900

    :sparkles: Feat : data context

---
+++ src/context/data.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export type DataState = { sickCd: string; sickNm: string }[];
+
+type DataSetState = React.Dispatch<React.SetStateAction<DataState>>;
+
+const DataStateContext = createContext<DataState | null>(null);
+const SetDataStateContext = createContext<DataSetState | null>(null);
+
+export function DataProvider({ children }: { children: React.ReactNode }) {
+  const [state, setState] = useState<DataState>([]);
+  return (
+    <DataStateContext.Provider value={state}>
+      <SetDataStateContext.Provider value={setState}>{children}</SetDataStateContext.Provider>
+    </DataStateContext.Provider>
+  );
+}
+
+export function useDataStateContext() {
+  const context = useContext(DataStateContext);
+
+  if (context == null) {
+    throw new Error(`Cannot find <DataStateContext.Provider>`);
+  }
+
+  return context;
+}
+
+export function useSetDataStateContext() {
+  const context = useContext(SetDataStateContext);
+
+  if (context == null) {
+    throw new Error(`Cannot find <SetDataStateContext.Provider>`);
+  }
+
+  return context;
+}
