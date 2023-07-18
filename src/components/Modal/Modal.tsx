76e0e3592975973fbc264a75fd40f6aa7b9100cd
jsdmas <sdmas5730@gmail.com>
2023-07-18 23:23:40 +0900

    :art: Style : modal style

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -25,11 +25,14 @@ const Layer = styled.div`
 `;
 const ModalLayer = styled.div`
   z-index: 2000;
-  width: 200px;
-  height: 300px;
-  position: fixed;
-  top: 50%;
+  width: 470px;
+  border-radius: 20px;
+  min-height: 200px;
+  position: absolute;
+  box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.1);
+  top: 62%;
   left: 50%;
+  background-color: white;
   transform: translate(-50%, -50%);
 `;
 export default Modal;
