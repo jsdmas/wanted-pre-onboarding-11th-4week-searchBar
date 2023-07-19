58ce546fe90eae48598da405488a0df697ee62d5
jsdmas <sdmas5730@gmail.com>
2023-07-19 16:42:56 +0900

    :sparkles: Feat : cache expire time 구현

--- src/apis/ServerApi.ts
+++ src/apis/ServerApi.ts
@@ -4,16 +4,25 @@ export const getVaildResponse = async (params: string) => {
   const query = new URLSearchParams({ q: params }).toString();
   const url = `${BASE_URL}?${query}`;
 
-  const cache = await caches.open('myCache'); // 캐시 저장소 열기
+  const cache = await caches.open('myCache');
 
   const cacheResponse = await caches.match(url);
-  return cacheResponse ? cacheResponse.json() : getFetchResponse(url, cache);
+
+  if (cacheResponse) {
+    const cachedData = await cacheResponse.json();
+    if (Date.now() - cachedData.timestamp < 10 * 1000) {
+      return cachedData.data;
+    }
+  }
+
+  return getFetchResponse(url, cache);
 };
 
 const getFetchResponse = async (url: string, cache?: Cache) => {
   const response = await fetch(url);
+  const data = await response.json();
 
-  cache?.put(url, response.clone()); // 캐시 저장
-
-  return response.json();
+  cache?.put(url, new Response(JSON.stringify({ data, timestamp: Date.now() })));
+  console.log('data-api 요청');
+  return data;
 };
